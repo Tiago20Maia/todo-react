49c113251236da6f5a232dc762356ae0b94ddb44
tiagolenovo <tiagoferreira209@gmail.com>
2024-04-24 00:29:17 -0300

    Criando listagem de tarefas

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,10 +1,20 @@
 import React, { useState } from 'react';
+import { MdDelete } from 'react-icons/md';
+
 import './App.css';
 
 const App = () => {
     const ESCAPE_KEY = 27;
     const ENTER_KEY = 13;
 
+    const initialTodos = [
+        { id: 1, title: 'Estudar React', checked: false },
+        { id: 2, title: 'Estudar JS', checked: true },
+        { id: 3, title: 'Estudar Node JS', checked: false },
+        { id: 4, title: 'Estudar Redux', checked: false },
+    ];
+
+    const [todos] = useState(initialTodos);
     const [value, setValue] = useState('');
 
     const erase = () => {
@@ -41,6 +51,16 @@ const App = () => {
                     onChange={onChange}
                     onKeyDown={onKeyDown}
                 />
+                <ul className="todo-list">
+                    {todos.map((todo) => (
+                        <li key={todo.id.toString()}>
+                            <span className='todo'>{todo.title}</span>
+                            <button className='remove' type="button">
+                                <MdDelete size={28} />
+                            </button>
+                        </li>
+                    ))}
+                </ul>
             </section>
         </section>
     );
