07741b84778d91658bb446e16ef9fbca4ca54c02
tiagolenovo <tiagoferreira209@gmail.com>
2024-04-23 22:14:21 -0300

    Ajuste do input

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,7 +1,48 @@
+import React, { useState } from 'react';
 import './App.css';
 
-const App = () => (
-    <h1 className='title'>TODO</h1>
-);
+const App = () => {
+    const ESCAPE_KEY = 27;
+    const ENTER_KEY = 13;
 
+    const [value, setValue] = useState('');
+
+    const erase = () => {
+        setValue('');
+    };
+
+    const submit = () => {
+        console.log(value);
+        erase();
+    };
+
+    const onChange = (e) => {
+        setValue(e.target.value);
+    };
+
+    const onKeyDown = (e) => {
+        if (e.which === ENTER_KEY) {
+            submit();
+        } else if (e.which === ESCAPE_KEY) {
+            erase();
+        }
+    };
+
+    return (
+        <section id="app" className="container">
+            <header>
+                <h1 className="title">todo</h1>
+            </header>
+            <section className="main">
+                <input
+                    className="new-todo"
+                    placeholder="o que precisa ser feito"
+                    value={value}
+                    onChange={onChange}
+                    onKeyDown={onKeyDown}
+                />
+            </section>
+        </section>
+    );
+};
 export default App;
